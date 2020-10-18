6c3ca43d2c55785911f2e4ade51bfa678353bcd0
swings <1321457973@qq.com>
2020-10-18 23:43:22 +0800

    feat: load kernel

--- build.sh
+++ build.sh
@@ -4,4 +4,9 @@ nasm -I boot/include/ -o boot/mbr.bin boot/mbr.S
 nasm -I boot/include/ -o boot/loader.bin boot/loader.S
 dd if=boot/mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
 dd if=boot/loader.bin of=hd60M.img bs=512 count=4 seek=2 conv=notrunc
+
+gcc -m32 -c -o kernel/main.o kernel/main.c
+ld kernel/main.o -melf_i386 -Ttext 0xc0001500 -e main -o kernel/kernel.bin
+dd if=kernel/kernel.bin of=hd60M.img bs=512 count=200 seek=9 conv=notrunc
+
 bochs -f bochsrc.disk
