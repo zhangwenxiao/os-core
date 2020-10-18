c376935f55eb0e15beb686370df0187a0b83187b
swings <1321457973@qq.com>
2020-10-18 17:33:25 +0800

    feat: page

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 nasm -I boot/include/ -o boot/mbr.bin boot/mbr.S
 nasm -I boot/include/ -o boot/loader.bin boot/loader.S
 dd if=boot/mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
-dd if=boot/loader.bin of=hd60M.img bs=512 count=2 seek=2 conv=notrunc
+dd if=boot/loader.bin of=hd60M.img bs=512 count=4 seek=2 conv=notrunc
+bochs -f bochsrc.disk
