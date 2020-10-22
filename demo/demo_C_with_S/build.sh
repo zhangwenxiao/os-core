a5a9397aa90dd783875101338d6510af90a589e7
swings <1321457973@qq.com>
2020-10-22 23:48:01 +0800

    feat: print.S

--- demo/demo_C_with_S/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-nasm -f elf -o C_with_S_S.o C_with_S_S.S
-gcc -m32 -c -o C_with_S_c.o C_with_S_c.c
-ld -melf_i386 -o C_with_S.bin C_with_S_S.o C_with_S_c.o
--- demo/demo_syscall_write/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nasm -f elf -o syscall_write.o syscall_write.S
-ld -melf_i386 -o syscall_write.bin syscall_write.o
