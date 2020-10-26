cbc872d81df66543c2ba222e34154ab57e57166b
swings <1321457973@qq.com>
2020-10-27 00:53:53 +0800

    feat: put_int

--- kernel/main.c
+++ kernel/main.c
@@ -2,5 +2,14 @@
 
 void main(void) {
     put_str("I am kernel\n");
+    put_int(0);
+    put_char('\n');
+    put_int(9);
+    put_char('\n');
+    put_int(0x00021a3f);
+    put_char('\n');
+    put_int(0x12345678);
+    put_char('\n');
+    put_int(0x00000000);
     while(1);
 }
--- lib/kernel/print.h
+++ lib/kernel/print.h
@@ -4,5 +4,6 @@
 #include "stdint.h"
 void put_char(uint8_t char_asci);
 void put_str(char* message);
+void put_int(uint32_t num); // 以十六进制打印
 
 #endif
