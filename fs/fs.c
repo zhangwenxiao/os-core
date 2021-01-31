e2724f4ec4e2f1c0f6f87424c26f36b23af606f5
swings <1321457973@qq.com>
2021-01-31 13:23:33 +0800

    feat: 分区挂载

--- fs/fs.c
+++ fs/fs.c
@@ -11,6 +11,49 @@
 #include "debug.h"
 #include "memory.h"
 
+struct partition* cur_part; // 默认情况下操作的是哪个分区
+
+// 在分区链表中找到名为 part_name 的分区, 并将其指针赋值给 cur_part
+static bool mount_partition(struct list_elem* pelem, int arg) {
+    char* part_name = (char*)arg;
+    struct partition* part = elem2entry(struct partition, part_tag, pelem);
+    if (!strcmp(part->name, part_name)) {
+        cur_part = part;
+        struct disk* hd = cur_part->my_disk;
+
+        // sb_buf 用来存储从硬盘上读入的超级块
+        struct super_block* sb_buf = (struct super_block*)sys_malloc(SECTOR_SIZE);
+
+        // 在内存中创建分区 cur_part 的超级块
+        cur_part->sb = (struct super_block*)sys_malloc(sizeof(struct super_block));
+        if (cur_part->sb == NULL) {
+            PANIC("alloc memory failed!");
+        }
+
+        // 读入超级块
+        memset(sb_buf, 0, SECTOR_SIZE);
+        ide_read(hd, cur_part->start_lba+1, sb_buf, 1);
+
+        // 把 sb_buf 中超级块的信息复制到分区的超级块 sb 中
+        memcpy(cur_part->sb, sb_buf, sizeof(struct super_block));
+
+        // 将硬盘上的块位图读入到内存
+        cur_part->block_bitmap.bits = (uint8_t*)sys_malloc(sb_buf->block_bitmap_sects*SECTOR_SIZE);
+        if (cur_part->block_bitmap.bits == NULL) {
+            PANIC("alloc memory failed!");
+        }
+        cur_part->block_bitmap.btmp_bytes_len = sb_buf->block_bitmap_sects * SECTOR_SIZE;
+        // 从硬盘上读入块位图到分区的 block_bitmap.bits
+        ide_read(hd, sb_buf->block_bitmap_lba, cur_part->block_bitmap.bits, sb_buf->block_bitmap_sects);
+
+        list_init(&cur_part->open_inodes);
+        printk("mount %s done!\n", part->name);
+
+        return true; // 使 list_traversal 停止遍历
+    }
+    return false; // 使 list_traversal 继续遍历
+}
+
 // 格式化分区, 也就是初始化分区的元信息, 创建文件系统
 static void partition_format(struct partition* part) {
     // 为方便实现, 一个块大小是一扇区
@@ -170,4 +213,9 @@ void filesys_init() {
         channel_no++; // 下一通道
     }
     sys_free(sb_buf);
+
+    // 确定默认操作的分区
+    char default_part[8] = "sdb1";
+    // 挂载分区
+    list_traversal(&partition_list, mount_partition, (int)default_part);
 }
